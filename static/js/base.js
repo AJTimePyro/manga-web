97ca539da0a21e0546f4ed7b4e42dbaa2ff0bdbf
Abhijeet <ajtimepyro@gmail.com>
2022-04-26 18:46:50 +0530

    fixed search not working after getting results

--- static/js/base.js
+++ static/js/base.js
@@ -6,29 +6,27 @@ function searchQuery(e) {
     query = document.querySelector('#search-box input').value;
     window.location.href = `?search=${query}`;
 }
-window.onload = () => {
 
-    // For enter in search box
-    document.getElementById(
-        "search-box"
-    ).addEventListener(
-        'keypress',
-        (e)=> {
-            if (e.key == "Enter") {
-                searchQuery(e);
-            };
-        }
-    );
-
-    // On clicking on search icon
-    document.getElementById(
-        "search-btn"
-    ).addEventListener(
-        'click',
-        (e)=> {
-            console.log('hi')
+// For enter in search box
+document.getElementById(
+    "search-box"
+).addEventListener(
+    'keypress',
+    (e)=> {
+        if (e.key == "Enter") {
             searchQuery(e);
-        }
-    );
-}
+        };
+    }
+);
+
+// On clicking on search icon
+document.getElementById(
+    "search-btn"
+).addEventListener(
+    'click',
+    (e)=> {
+        console.log('hi')
+        searchQuery(e);
+    }
+);
 
